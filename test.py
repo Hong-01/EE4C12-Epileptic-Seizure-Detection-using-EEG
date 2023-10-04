1b8e0f0843957ea42fc98889ebdf8171c69f9615
Yanqi Hong <88537757+Hong-01@users.noreply.github.com>
2023-10-04 11:50:37 +0200

    test

--- test.py
+++ test.py
@@ -1,2 +1,5 @@
+import os
 print("hello world")
 #你好，世界
+data_dir = os.path.join(os.getcwd(), 'E:\DATA\TUD\Master\TUD_Master_Y1\Q1\EE4C12 Machine Learning For Electrical Engineering\CodeLab\Lab4\dataset\\')
+print('Number of images in the dataset folder: ', len(os.listdir(data_dir)))
